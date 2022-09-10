13f6a077844f3e5ba792a1d726c3ee3279cb95d5
J. Nick Koston <nick@koston.org>
2022-09-10 08:46:23 -0500

    feat: export get_device from bleak-retry-connector

--- src/led_ble/__init__.py
+++ src/led_ble/__init__.py
@@ -3,7 +3,15 @@ from __future__ import annotations
 __version__ = "0.7.1"
 
 
+from bleak_retry_connector import get_device
+
 from .exceptions import CharacteristicMissingError
 from .led_ble import BLEAK_EXCEPTIONS, LEDBLE, LEDBLEState
 
-__all__ = ["BLEAK_EXCEPTIONS", "CharacteristicMissingError", "LEDBLE", "LEDBLEState"]
+__all__ = [
+    "BLEAK_EXCEPTIONS",
+    "CharacteristicMissingError",
+    "LEDBLE",
+    "LEDBLEState",
+    "get_device",
+]
